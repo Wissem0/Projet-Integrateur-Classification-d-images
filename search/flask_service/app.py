0f0006792d172da210d1d3a7427fba135d0d0c05
BadrNadi <badrnadi18@gmail.com>
2023-01-21 21:05:25 +0100

    cnn/knn deployment

--- search/flask_service/app.py
+++ search/flask_service/app.py
@@ -13,10 +13,11 @@ PATH_TO_CSV = ['list_attr_celeba.csv',
 
 @app.route("/search/<user_vector>", methods=['POST', 'GET'])
 def search(user_vector: str):
-    v = [Attribute(k) for k in user_vector]
-    s = knn(5, dataset, Example("user", v))
-    print(str(v))
-    return str(s)
+    # v = [Attribute(k) for k in user_vector]
+    # s = knn(5, dataset, Example("user", v))
+    # print(str(v))
+
+    return user_vector + 'received'
 
 
 @app.route("/")
@@ -53,7 +54,7 @@ if __name__ == "__main__":
             raise ValueError('Example <'+example.name+'> has ' + str(len(example.attributes)) +
                              ' attribute(s): ' + str(len(features)) + ' required. (len(attributes) = '+str(len(attributes))+')')
     print("Loaded "+str(len(example_list))+" examples. \nBuilding dataset ...")
-    dataset = Da5000taset(features, example_list)
+    dataset = Dataset(features, example_list)
     print("Dataset build.")
     print(str(dataset))
     app.run(debug=True, host='0.0.0.0', port=5000)
