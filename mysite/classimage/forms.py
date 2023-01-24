279204d1fa7f509f16d7d1bded5bc780a6f5abb2
“Jean6-971” <75419220+Jean6-971@users.noreply.github.com>
2023-01-24 18:09:07 +0100

    Deleted mySite

--- mysite/classimage/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import *
- 
- 
-class FaceForm(forms.ModelForm):
- 
-    class Meta:
-        model = Face
-        fields = ['name', 'face_Main_Img']
--- mysite/classimage/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Face(models.Model):
-    name = models.CharField(max_length=50)
-    face_Main_Img = models.ImageField(upload_to='images/')
