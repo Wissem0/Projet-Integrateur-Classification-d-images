6050539acd52704f435e964fc1d0ef856a6a049f
scanim-valentin <guerbett@etud.insa-toulouse.fr>
2023-01-25 18:39:36 +0100

    Cleaned search

--- search/flask_service/test_model.py
+++ search/flask_service/test_model.py
@@ -1,33 +1,37 @@
 #!/usr/bin/env python3
-import pandas, os
 from model import Attribute, Example, Dataset
-PATH_TO_CSV = ['list_attr_celeba.csv','../list_attr_celeba.csv','../../list_attr_celeba.csv',]
-if __name__ == "__main__":
-    
-    path = None
-    for trypath in PATH_TO_CSV : 
-        if os.path.isfile(trypath) :
-            path = trypath
-            break
-    if not path : 
-        raise FileExistsError("No file found in any of the following location: \n"+str(PATH_TO_CSV))
-    
-    print("Loading "+path+" ...")
-    csvFile = pandas.read_csv(path)
-    print("Listing features ...")
-    features = list(csvFile.axes[1][1:])
-    print("Found "+str(len(features))+" features. \nLoading image names . . .")
-    names = [str(n) for n in csvFile.get('image_id')]
-    print("Found "+str(len(names))+" image names.")
-    example_list = []
-    print("Loading examples ...")
-    for raw_example in csvFile.iloc:
-        attributes = [Attribute('0') if atr==-1 else Attribute('1') for atr in raw_example[1:]]
-        example = Example(raw_example[0],attributes)
-        example_list.append(example)
-        if(len(example.attributes)>40) : 
-            raise ValueError('Example <'+example.name+'> has ' + str(len(example.attributes)) + ' attribute(s): ' + str(len(features)) + ' required. (len(attributes) = '+str(len(attributes))+')')
-    print("Loaded "+str(len(example_list))+" examples. \nBuilding dataset ...")
-    dataset = Dataset(features,example_list)
-    print("Dataset build.")
-    print(str(dataset))
+import random
+
+def generate_example(nb_features: int) -> Example:
+    return Example("Random Example",[Attribute(str(random.randint(0, 1))) for j in range(nb_features)])
+
+# Testing various properties for the Hamming distance
+class TestHamming:
+
+    # e1 = e2 <=> d(e1,e2) = 0
+    def test_separation(self):
+        for k in range(5,200,10):
+            e1 = generate_example(k)
+            e2 = generate_example(k)
+            d = Dataset([str(f) for f in range(k)], [e1,e2], [random.randint(0,10) for _ in range(k)])
+            # Vectors are either different or their distance is 0
+            assert (d.hamming_distance(e1,e2) == 0) != (e1.attributes != e2.attributes)
+
+    # d(e1,e2) = d(e2,e1)
+    def test_symmetry(self):
+        for k in range(5, 200, 10):
+            e1 = generate_example(k)
+            e2 = generate_example(k)
+            d = Dataset([str(f) for f in range(k)], [e1, e2], [random.randint(0,10) for _ in range(k)])
+            # Vectors can be mirrored in the function
+            assert d.hamming_distance(e1,e2) == d.hamming_distance(e2,e1)
+
+    # d(e1, e3) <= d(e1,e2) + d(e2,e3)
+    def test_triangle_inequality(self):
+        for k in range(5, 200, 10):
+            e1 = generate_example(k)
+            e2 = generate_example(k)
+            e3 = generate_example(k)
+            d = Dataset([str(f) for f in range(k)], [e1, e2, e3], [random.randint(0,10) for _ in range(k)])
+            # Distance respects triangle inequality
+            assert d.hamming_distance(e1,e3) <= d.hamming_distance(e1,e2) + d.hamming_distance(e2,e3)
